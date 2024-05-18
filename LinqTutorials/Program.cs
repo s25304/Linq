3f6fb696cf9e5fe7e4a332b68599215998a71966
s25304 <s25304@pjwstk.edu.pl>
2024-05-18 23:26:12 +0200

    init

---
+++ LinqTutorials/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = LinqTasks.Task1();
+            Console.log(t);
+
+        }
+    }
+}
